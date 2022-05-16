a324eca5529dbfc16b6a030a0ff218e66e0b00e5
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-05-16 12:13:38 -0400

    Add project files.

---
+++ BlazorWindowManager.ClassLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWindowManager.ClassLibrary;
+
+public static class BlazorWindowManagerClassLibraryServiceProvider
+{
+    public static IServiceCollection AddBlazorWindowManagerClassLibraryServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
