da5854522ee6a31bbc07ce06db4d7b866402d007
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-05-17 12:58:51 -0400

    Click Here to Add Window to this grid

--- BlazorWindowManager.RazorClassLibrary/Grid/GridDisplay.razor.cs
+++ BlazorWindowManager.RazorClassLibrary/Grid/GridDisplay.razor.cs
@@ -7,15 +7,27 @@ using Microsoft.AspNetCore.Components.Web;
 using BlazorWindowManager.RazorClassLibrary.Icons.Codicon;
 using BlazorWindowManager.ClassLibrary.Grid;
 using BlazorWindowManager.ClassLibrary.Dimension;
+using BlazorWindowManager.ClassLibrary.Store.WindowManagerDialog;
+using BlazorWindowManager.ClassLibrary.WindowManagerDialog;
+using Fluxor;
 
 namespace BlazorWindowManager.RazorClassLibrary.Grid;
 
 public partial class GridDisplay : ComponentBase
 {
+    [Inject]
+    private IViewportDimensionsService ViewportDimensionsService { get; set; } = null!;
+    [Inject]
+    private IDispatcher Dispatcher { get; set; } = null!;
+
     [Parameter, EditorRequired]
     public GridRecord GridRecord { get; set; } = null!;
     [Parameter, EditorRequired]
     public DimensionsRecord InitialDimensionsRecord { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public Type AddWindowToGridRenderedType { get; set; } = null!;
+    [Parameter, EditorRequired]
+    public Dictionary<string, object> AddWindowToGridRenderedTypeParameters { get; set; } = null!;
     [Parameter]
     public bool IsResizable { get; set; } = false;
 
@@ -31,5 +43,20 @@ public partial class GridDisplay : ComponentBase
     private void OnDimensionsRecordChangedEventCallback(DimensionsRecord dimensionsRecord)
     {
         _dimensionsRecord = dimensionsRecord;
+    }
+
+    private async Task OnAddWindowToGridOnClickAsync()
+    {
+        var dimensionsRecordForDialog = await WindowManagerDialogRecord.ConstructDefaultDimensionsRecord(ViewportDimensionsService);
+
+        var windowManagerDialogRecord = new WindowManagerDialogRecord(Guid.NewGuid(),
+            "Add Window To Grid",
+            AddWindowToGridRenderedType,
+            AddWindowToGridRenderedTypeParameters,
+            dimensionsRecordForDialog);
+
+        var action = new AddWindowManagerDialogRecordAction(windowManagerDialogRecord);
+
+        Dispatcher.Dispatch(action);
     }
 }
