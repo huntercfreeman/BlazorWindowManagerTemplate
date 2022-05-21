4b3f024f8d61950bcac3557e92b5da0786457bd0
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-05-21 17:43:44 -0400

    Drag event provider is bugging out and not working randomly

---
+++ BlazorWindowManagerWebAssemblyShowcase/Program.cs
@@ -0,0 +1,16 @@
+using BlazorWindowManager.ClassLibrary.Theme;
+using BlazorWindowManager.RazorClassLibrary;
+using BlazorWindowManagerWebAssemblyShowcase;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorWindowManagerRazorClassLibraryServices(
+    BlazorWindowManagerThemeKind.LightTheme);
+
+await builder.Build().RunAsync();
